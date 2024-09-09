ef2bb34de9c53bdca5c361882b1fe6b08a3d8d31
paololalap <paolomorales1502@gmail.com>
2024-09-09 21:49:30 +0800

    move the lanes and roles in constants folder

---
+++ src/constants/LANES.ts
@@ -0,0 +1,9 @@
+const LANES = [
+  { src: "/lanes/gold-lane.png", title: "Gold Lane", alt: "gold lane" },
+  { src: "/lanes/exp-lane.png", title: "Exp Lane", alt: "exp lane" },
+  { src: "/lanes/mid-lane.png", title: "Mid Lane", alt: "mid lane" },
+  { src: "/lanes/roamer.png", title: "Roamer", alt: "roamer" },
+  { src: "/lanes/jungler.png", title: "Jungler", alt: "jungler" },
+];
+
+export { LANES };
---
+++ src/constants/ROLES.ts
@@ -0,0 +1,10 @@
+const ROLES = [
+  { src: "/roles/tank-role.png", title: "Tank", alt: "tank" },
+  { src: "/roles/support-role.png", title: "Support", alt: "support" },
+  { src: "/roles/marksman-role.png", title: "Marksman", alt: "marksman" },
+  { src: "/roles/mage-role.png", title: "Mage", alt: "mage" },
+  { src: "/roles/assassin-role.png", title: "Assassin", alt: "assassin" },
+  { src: "/roles/fighter-role.png", title: "Fighter", alt: "fighter" },
+];
+
+export { ROLES };
