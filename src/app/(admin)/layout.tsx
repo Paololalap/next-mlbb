10ea8299b77489df872933603be335a9e3ff1e9a
paololalap <paolomorales1502@gmail.com>
2024-09-25 21:12:29 +0800

    edit layout

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,14 +1,13 @@
 import { AuthProvider } from "@/providers/providers";
-import { ReactNode } from "react";
 
 export default async function AdminLayout({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
 }) {
   return (
-    <div className="container pt-10">
-      <AuthProvider>{children}</AuthProvider>
-    </div>
+    <AuthProvider>
+      <div className="container pt-10">{children}</div>
+    </AuthProvider>
   );
 }
