cab9c718e4b82674e68025ddae820b13b52ccf76
paololalap <paolomorales1502@gmail.com>
2024-09-30 21:27:54 +0800

    feat: add tooltip

--- src/components/MLCharacterList.tsx
+++ src/components/MLCharacterList.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, CircleHelp } from "lucide-react";
+import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
@@ -19,6 +20,13 @@ import { LANES } from "@/constants/LANES";
 import { ROLES } from "@/constants/ROLES";
 import { WEEKS } from "@/constants/WEEKS";
 import { laneMapping, useCharacter } from "@/stores/useCharacter";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 
 interface MLCharacterListProps {
   children: React.ReactNode;
@@ -78,42 +86,64 @@ const MLCharacterList = ({
           <div className="flex items-center gap-x-3">
             <CardTitle>{title}</CardTitle>
             {toggleWeeks && (
-              <div className="relative" ref={dropdownRef}>
-                <motion.button
-                  onClick={() => setIsOpen(!isOpen)}
-                  className="flex h-10 w-[160px] items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                >
-                  <span className="truncate">{selectedWeekLabel}</span>
-                  <motion.div
-                    animate={{ rotate: isOpen ? 180 : 0 }}
-                    transition={{ duration: 0.3 }}
-                    className="ml-2 flex-shrink-0"
+              <>
+                <div className="relative" ref={dropdownRef}>
+                  <motion.button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="flex h-10 w-[160px] items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                   >
-                    <ChevronDown className="size-4 opacity-50" />
-                  </motion.div>
-                </motion.button>
-                <AnimatePresence>
-                  {isOpen && (
-                    <motion.ul
-                      initial={{ opacity: 0, y: -10 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      exit={{ opacity: 0, y: -10 }}
-                      transition={{ duration: 0.2 }}
-                      className="absolute z-10 mt-2 w-[160px] rounded-md border bg-popover text-popover-foreground shadow-md"
+                    <span className="truncate">{selectedWeekLabel}</span>
+                    <motion.div
+                      animate={{ rotate: isOpen ? 180 : 0 }}
+                      transition={{ duration: 0.3 }}
+                      className="ml-2 flex-shrink-0"
                     >
-                      {sortedWeeks.map((week) => (
-                        <motion.li
-                          key={week.week}
-                          className="flex h-10 w-[160px] cursor-pointer items-center whitespace-nowrap px-3 text-sm hover:bg-accent hover:text-accent-foreground"
-                          onClick={() => handleWeekChange(week.week)}
+                      <ChevronDown className="size-4 opacity-50" />
+                    </motion.div>
+                  </motion.button>
+                  <AnimatePresence>
+                    {isOpen && (
+                      <motion.ul
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        transition={{ duration: 0.2 }}
+                        className="absolute z-10 mt-2 w-[160px] rounded-md border bg-popover text-popover-foreground shadow-md"
+                      >
+                        {sortedWeeks.map((week) => (
+                          <motion.li
+                            key={week.week}
+                            className="flex h-10 w-[160px] cursor-pointer items-center whitespace-nowrap px-3 text-sm hover:bg-accent hover:text-accent-foreground"
+                            onClick={() => handleWeekChange(week.week)}
+                          >
+                            Week {week.week}
+                          </motion.li>
+                        ))}
+                      </motion.ul>
+                    )}
+                  </AnimatePresence>
+                </div>
+
+                <TooltipProvider delayDuration={0}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <CircleHelp className="size-4 text-muted-foreground" />
+                    </TooltipTrigger>
+                    <TooltipContent side="right" className="max-w-52">
+                      <p>
+                        This was based on{" "}
+                        <Link
+                          href="https://www.youtube.com/@MPLPhilippines"
+                          target="_blank"
                         >
-                          Week {week.week}
-                        </motion.li>
-                      ))}
-                    </motion.ul>
-                  )}
-                </AnimatePresence>
-              </div>
+                          MPL Philippines
+                        </Link>{" "}
+                        Picks and Bans
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </>
             )}
           </div>
           <Search value={searchQuery} onChange={setSearchQuery} />
