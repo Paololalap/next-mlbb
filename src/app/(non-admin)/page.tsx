e44bf15719c762dd47a87daf8dfa1c93bc111ea6
paololalap <paolomorales1502@gmail.com>
2024-10-03 20:05:58 +0800

    chore: commented unused code

--- src/app/(non-admin)/page.tsx
+++ src/app/(non-admin)/page.tsx
@@ -1,16 +1,16 @@
 import { MLCharacterListWrapper } from "@/components/MLCharacterListWrapper";
 import { CHARACTERS } from "@/constants/CHARACTERS";
 
-export async function getCharacters() {
+/* export async function getCharacters() {
   const res = await fetch("http://localhost:3000/api/characters");
   if (!res.ok) {
     throw new Error("Failed to fetch characters");
   }
   const data = await res.json();
   return data.characters;
-}
+} */
 
-export default async function Homepage() {
+export default function Homepage() {
   // const characters = await getCharacters();
 
   return (
